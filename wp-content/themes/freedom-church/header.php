7dffc71c3c6a15b88de5f4fe8a6202a37ab4d953
Jesse Kade <jessekade@Sharp-Machine.local>
2012-08-30 12:59:18 -0700

    added favicon

--- wp-content/themes/freedom-church/header.php
+++ wp-content/themes/freedom-church/header.php
@@ -39,7 +39,7 @@
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 
     	<!-- Le fav and touch icons -->
-    	<link rel="shortcut icon" href="../assets/ico/favicon.ico">
+    	<link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/img/ico/favicon.ico">
     	<link rel="apple-touch-icon-precomposed" sizes="144x144" href="../assets/ico/apple-touch-icon-144-precomposed.png">
     	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="../assets/ico/apple-touch-icon-114-precomposed.png">
     	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="../assets/ico/apple-touch-icon-72-precomposed.png">
