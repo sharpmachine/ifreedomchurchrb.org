3052fe81b1897635d4424aa8084d2aa07301ae30
Jesse Kade <jessekade@Sharp-Machine.local>
2012-09-20 16:59:37 -0700

    upgrade analytics plugin

--- wp-content/themes/freedom-church/503.php
+++ wp-content/themes/freedom-church/503.php
@@ -7,15 +7,15 @@
 <style type="text/css">
 	body
 	{
-		background-color: #000;
-		color: #fff;
+		background-color: #fff;
+		color: #000;
 		font-family: Arial, Helvetica, sans-serif;
 	}
 	
 	#logo
 	{
-		width: 355px;
-		margin: 50px auto;
+		width: 176px;
+		margin: 104px auto;
 	}
 	
 	p
@@ -29,7 +29,7 @@
 <body>
 		
 	<div id="logo">
-		<img src="http://www.domain.com/wp-content/themes/project_theme/images/logo.png" width="335" height="52" alt="Logo">
+		<img src="http://ifreedomchurchrb.org/wp-content/themes/freedom-church/img/logo.png" width="176" height="104" alt="Logo">
 		<p>We are currently undergoing routine maintenance.  Please check back soon.</p>
 	</div>
 		
