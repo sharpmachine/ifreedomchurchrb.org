8b90a0f302c3328468e0146fd135c7df5a917d67
Jesse Kade <jessekade@Sharp-Machine.local>
2012-09-25 12:28:50 -0700

    updated giving page per their request

--- wp-content/themes/freedom-church/page-giving.php
+++ wp-content/themes/freedom-church/page-giving.php
@@ -27,11 +27,24 @@
 			<?php endwhile; endif; ?>
 			</div>
 			
-			<div id="one-time-donations">
+			<div id="monthly-parnter" class="row">
+				<div class="span4"><h3 class="black">Monthly Partner</h3></div>
+				<div class="span12">
+					<?php while(the_repeater_field('monthly_partner')): ?>
+						<a href="<?php the_sub_field('monthly_partner_paypal_url'); ?>" class="btn btn-large"><?php the_sub_field('monthly_partner_button_label'); ?></a>
+					<?php endwhile; ?>
+				</div>
+			</div>
+			
+			<div id="one-time-donations" class="row">
+				<div class="span5"><h3 class="black">One-Time Donations</h3></div>
+				<div class="span11">
 				<?php while(the_repeater_field('one-time_donation_buttons')): ?>
 					<a href="<?php the_sub_field('one_time_paypal_url'); ?>" class="btn btn-large"><?php the_sub_field('one_time_button_label'); ?></a>
 				<?php endwhile; ?>
-				</div><!-- #one-time-donations -->
+				</div>
+			</div><!-- #one-time-donations -->
+				
 				<div class="row">
 					<div class="feature-box">
 						<div class="span9"><img src="<?php the_field('mission_partners_logo_or_photo'); ?>" alt="<?php the_field('mission_partners'); ?>"></div>
@@ -41,16 +54,6 @@
 						</div>
 					</div>
 				</div>
-			
-			
-			<div id="monthly-parnter" class="row">
-				<div class="span4"><h3 class="black">Monthly Partner</h3></div>
-				<div class="span12">
-					<?php while(the_repeater_field('monthly_partner')): ?>
-						<a href="<?php the_sub_field('monthly_partner_paypal_url'); ?>" class="btn btn-large"><?php the_sub_field('monthly_partner_button_label'); ?></a>
-					<?php endwhile; ?>
-				</div>
-			</div>
 			
 			<img title="Card Logos" src="<?php bloginfo('template_directory'); ?>/img/cards.png" alt="Card Logos" width="144" height="21" /> <span class="paypal-notice">All payments are process by</span> <img title="Paypal Logo" src="<?php bloginfo('template_directory'); ?>/img/paypal.png" alt="Paypal Logo" width="74" height="21" />
 			
